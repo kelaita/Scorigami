a20c6ad52d6b3c1f7d52b5294a92b864e98a6811
Paul Kelaita <paul@kelaita.com>
2022-10-22 21:28:35 -0700

    Working prototype

--- Scorigami/ContentView.swift
+++ Scorigami/ContentView.swift
@@ -9,31 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var viewModel: ScorigamiViewModel = ScorigamiViewModel()
-    @State var gameInfo: String = "Game Info"
+    @State var showingAlert: Bool = false
+    @State var score: String = ""
+    @State var occurrences: Int = 0
     
     var body: some View {
         
         ScrollView([.horizontal, .vertical]) {
-            Text(gameInfo)
             VStack {
                 ForEach(0...51, id: \.self) { losingScore in
                     LazyHGrid(rows: [GridItem(.adaptive(minimum: 20), spacing: 2)]) {
                         let row = viewModel.getGamesForLosingScore(losingScore: losingScore)
                         ForEach(row, id: \.self) { cell in
-                            let _ = print("grabbing losing score \(cell.id), \(cell.color)")
-                            //Text(cell.id)
                             Rectangle()
                                 .fill(cell.color)
                                 .frame(width: 10, height: 10)
                                 .padding(1)
                                 .gesture(TapGesture().onEnded {
-                                    let _ = print(cell.id, cell.occurrences)
-                                    gameInfo = cell.id + ", " + String(cell.occurrences) + " times"
+                                    self.score = cell.id
+                                    self.occurrences = cell.occurrences
+                                    self.showingAlert = true
                                 })
                         }
                     }
                 }
-            }
+            }.alert(isPresented: $showingAlert, content: {
+                Alert(title: Text("Game Score: " + score), message: Text("It happened " + String(occurrences) + " times"), dismissButton: .default(Text("OK")))
+            })
 
         }.padding(.all, 2.0)
 

c64e8f4d6cd725cfdb65d55e3f5b36240cabdec2
Paul Kelaita <paul@kelaita.com>
2022-11-01 22:27:53 -0700

    Fleshed out the About Page

--- Scorigami/AboutView.swift
+++ Scorigami/AboutView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct AboutView: View {
     var body: some View {
         VStack {
-            Text("🏈 Welcome to Scorigami 🏈")
-            Text("This is all about Scorigami")
-            Text("Who came up with the term")
-            Text("Data thanks to Pro-Football-Reference.com")
-            Text("Go see my GitHub")
+            Text("🏈 Welcome to Scorigami 🏈\n").frame(alignment: .leading).font(.system(size:24))
+            Text("A Scorigami, a term originally coined and defined by sportswriter Jon Bois, is a final score of an NFL game that has never occurred.\n").frame(maxWidth: .infinity, alignment: .leading)
+            Text("This app let's you browse every score combination and see which are Scorigamis (the black cells). For those final scores that have occurred at least once, you can see how many games ended in that score, and when it last happened.\n").frame(maxWidth: .infinity, alignment: .leading)
+            Text("You can also see detailed information of every single game by tapping on each score's 'View games' link.\n").frame(maxWidth: .infinity, alignment: .leading)
+            Text("Thank you to pro-football-reference.com who provided all the historical data.\n\n\n\n").frame(maxWidth: .infinity, alignment: .leading)
+            Text("This is a work-in-progress app and was created in order to learn iOS app development with SwiftUI. Would love to get some feedback at https://github.com/kelaita/scorigami").frame(maxWidth: .infinity, alignment: .leading).font(.system(size: 14))
         }.navigationBarTitle("About Scorigami")
     }
 }
